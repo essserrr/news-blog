8e9a3dffbf095500606f0dc192c43c59ae8f8855
esser <blizzz93@gmail.com>
2022-02-06 17:58:15 +0900

    ordering added

--- src/db/queries/tags.ts
+++ src/db/queries/tags.ts
@@ -11,8 +11,10 @@ const tags = {
         ${TagsTable.NAME} text NOT NULL,
         PRIMARY KEY (${TagsTable.ID})
     );
-    CREATE UNIQUE INDEX IF NOT EXISTS ${TagsTable.NAME}_lower_unique 
-        ON ${Tables.TAGS} (lower(${TagsTable.NAME}));`,
+
+    CREATE UNIQUE INDEX IF NOT EXISTS ${TagsTable.NAME}_lower_unique  ON ${Tables.TAGS} (lower(${TagsTable.NAME}));
+    CREATE INDEX IF NOT EXISTS ${TagsTable.ID}_asc_index ON ${Tables.TAGS} (${TagsTable.ID} ASC);
+`,
 
   insert: `INSERT INTO ${Tables.TAGS}(${TagsTable.NAME}) VALUES ($1) RETURNING *;`,
 
@@ -20,7 +22,7 @@ const tags = {
 
   delete: `DELETE FROM ${Tables.TAGS}	WHERE ${TagsTable.ID}=$1;`,
 
-  selectAll: `SELECT * FROM ${Tables.TAGS};`,
+  selectAll: `SELECT * FROM ${Tables.TAGS} ORDER BY ${TagsTable.ID} ASC;`,
 
   select: `SELECT * FROM ${Tables.TAGS} WHERE ${TagsTable.ID}=$1`,
 } as const;
