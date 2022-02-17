6b583fa0557d940b69c391a474529c9cbe3b0f23
esser <blizzz93@gmail.com>
2022-02-17 13:40:59 +0900

    Author queries added

---
+++ src/db/queries/authors.ts
@@ -0,0 +1,52 @@
+import { Tables } from './tables';
+import { UsersTable } from './users';
+
+enum AuthorsTable {
+  UID = 'uid',
+  DESCRIPTION = 'description',
+}
+
+enum AuthorRules {
+  SORT_BY_UID = 'users_uid_sort_asc',
+}
+
+const CURRENT_TABLE = Tables.AUTHORS;
+
+const authors = {
+  createAuthorsTable: `CREATE TABLE IF NOT EXISTS ${CURRENT_TABLE}(
+        ${AuthorsTable.UID} bigserial NOT NULL PRIMARY KEY REFERENCES ${Tables.USERS} (${UsersTable.UID}) ON DELETE CASCADE,
+        ${AuthorsTable.DESCRIPTION} text
+    );
+
+    CREATE INDEX IF NOT EXISTS ${AuthorRules.SORT_BY_UID} ON ${CURRENT_TABLE} USING btree (${AuthorsTable.UID} ASC);
+`,
+
+  add: `INSERT INTO ${CURRENT_TABLE}(${AuthorsTable.UID}, ${AuthorsTable.DESCRIPTION}) VALUES ($1, $2) RETURNING *;`,
+
+  delete: `DELETE FROM ${CURRENT_TABLE} WHERE ${AuthorsTable.UID}=$1;`,
+
+  select: `SELECT * FROM ${CURRENT_TABLE} WHERE ${AuthorsTable.UID}=$1;`,
+
+  update: `
+              UPDATE ${CURRENT_TABLE}
+                SET ${AuthorsTable.DESCRIPTION} = CASE 
+                                            WHEN $3='(null_value)' THEN NULL
+                                            ELSE COALESCE($4, ${AuthorsTable.DESCRIPTION})
+                                          END
+                WHERE ${AuthorsTable.UID}=$1 
+                RETURNING *;`,
+
+  selectAll: `
+              SELECT
+                (SELECT COUNT(*) FROM ${CURRENT_TABLE}) as count, 
+                (SELECT json_agg(t.*) 
+                  FROM (
+                    SELECT * FROM ${CURRENT_TABLE}
+                    ORDER BY ${AuthorsTable.UID} ASC 
+                    LIMIT $1 
+                     OFFSET $2
+                  ) AS t
+                ) AS rows;`,
+} as const;
+
+export { AuthorsTable, AuthorRules, authors };
--- src/db/queries/index.ts
+++ src/db/queries/index.ts
@@ -1,13 +1,15 @@
 import { tags, TagsTable } from './tags';
 import { categories, CategoriesTable } from './categories';
 import { users, UsersTable } from './users';
+import { authors, AuthorsTable } from './authors';
 
 const queries = {
   tags,
   categories,
   users,
+  authors,
 } as const;
 
 export default queries;
 export { Tables } from './tables';
-export { TagsTable, CategoriesTable, UsersTable };
+export { TagsTable, CategoriesTable, UsersTable, AuthorsTable };
