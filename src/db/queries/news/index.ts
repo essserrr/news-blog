b5662ac07a75f1ee361e988cd743ed3bb5de68b2
esser <blizzz93@gmail.com>
2022-02-22 18:26:04 +0900

    test replace query added

--- src/db/queries/news/index.ts
+++ src/db/queries/news/index.ts
@@ -208,15 +208,87 @@ const news = {
   delete: `DELETE FROM ${Tables.NEWS} WHERE ${NewsTable.ID}=$1;`,
 
   update: `
-              UPDATE ${Tables.NEWS}
-                SET 
-                    ${NewsTable.AUTHOR} = COALESCE($2, ${NewsTable.AUTHOR}),
-                    ${NewsTable.TITLE} = COALESCE($3, ${NewsTable.TITLE}),
-                    ${NewsTable.CONTENT} = COALESCE($4, ${NewsTable.CONTENT}),
-                    ${NewsTable.CATEGORY} = COALESCE($5, ${NewsTable.CATEGORY}),
-                    ${NewsTable.MAIN_IMAGE} = COALESCE($6, ${NewsTable.MAIN_IMAGE})
-                WHERE ${NewsTable.ID}=$1 
-                RETURNING *;`,
+  WITH RECURSIVE
+    ${Parts.BODY} AS ( 
+        UPDATE ${Tables.NEWS} SET
+          ${NewsTable.TITLE}=$3, 
+          ${NewsTable.CONTENT}=$4, 
+          ${NewsTable.CATEGORY}=$5, 
+          ${NewsTable.MAIN_IMAGE}=$6
+        WHERE (${NewsTable.ID}=$1 AND ${NewsTable.AUTHOR}=$2 ) RETURNING *;
+      ),
+
+    test_delete AS (
+      DELETE FROM ${Tables.NEWS_TAGS} WHERE ${TagsSubTable.NID}=$1
+    ),
+
+    ${Parts.TAGS} AS (
+      INSERT INTO ${Tables.NEWS_TAGS} (${TagsSubTable.NID}, ${TagsSubTable.ID}) 
+      SELECT ${bodyPart.ID}, unnest($7::integer[]) FROM ${Parts.BODY} 
+      RETURNING ${TagsSubTable.ID}
+    ),
+
+    ${Parts.TAGS_FULL} AS (
+      SELECT * FROM ${Parts.TAGS} 
+      LEFT JOIN ${Tables.TAGS}
+        ON ${tagsPart.ID} = ${Tables.TAGS}.${TagsTable.ID}
+    ),
+
+    ${Parts.CATEGORIES} AS (
+      SELECT 
+        ${Tables.CATEGORIES}.${CategoriesTable.ID},
+        ${Tables.CATEGORIES}.${CategoriesTable.PID},
+        ${Tables.CATEGORIES}.${CategoriesTable.NAME},
+        1 AS ${Recursive.LEVEL}
+      FROM ${Parts.BODY} 
+      LEFT JOIN ${Tables.CATEGORIES}
+        ON ${bodyPart.CATEGORY} = ${Tables.CATEGORIES}.${CategoriesTable.ID}
+      
+    
+      UNION ALL
+    
+      SELECT 
+        ${Tables.CATEGORIES}.${CategoriesTable.ID}, 
+        ${Tables.CATEGORIES}.${CategoriesTable.PID}, 
+        ${Tables.CATEGORIES}.${CategoriesTable.NAME}, 
+        ${categoriesPart.LEVEL} + 1 AS ${Recursive.LEVEL}
+      FROM ${Tables.CATEGORIES}
+      JOIN ${Parts.CATEGORIES} 
+      ON 
+        ${Tables.CATEGORIES}.${CategoriesTable.ID} 
+        = 
+        ${Parts.CATEGORIES}.${CategoriesTable.PID}
+    ),
+
+    test_delete2 AS (
+      DELETE FROM ${Tables.NEWS_IMAGES} WHERE ${ImagesSubTable.NID}=$1
+    ),
+
+    ${Parts.IMAGES} AS (
+      INSERT INTO ${Tables.NEWS_IMAGES} (${ImagesSubTable.NID}, ${ImagesSubTable.PATH}) 
+      SELECT ${bodyPart.ID}, unnest($8::text[]) FROM ${Parts.BODY} 
+      RETURNING ${ImagesSubTable.PATH}
+    ),
+
+    ${Parts.AUTHOR} AS (
+      SELECT ${author} FROM ${Parts.BODY} 
+
+      LEFT JOIN ${Tables.AUTHORS}
+        ON ${bodyPart.AUTHOR} = ${Tables.AUTHORS}.${AuthorsTable.UID}
+      LEFT JOIN ${Tables.USERS}
+        ON ${bodyPart.AUTHOR} = ${Tables.USERS}.${UsersTable.UID}
+    )
+
+
+    
+  SELECT 
+    ${bodyPart.ALL}, ${timestampToInteger(`${bodyPart.CREATED_AT}`, NewsTable.CREATED_AT)},
+    (SELECT to_json(${authorPart.ALL}) FROM ${Parts.AUTHOR}) AS ${NewsTable.AUTHOR},
+    (SELECT json_agg(${categoriesPart.ALL}) 
+        FROM ${Parts.CATEGORIES}) AS ${NewsTable.CATEGORY},
+    (SELECT json_agg(${imagesPart.ALL}) FROM ${Parts.IMAGES}) AS ${NewsFields.AUX_IMAGES},
+    (SELECT json_agg(${tagsFullPart.ALL}) FROM ${Parts.TAGS_FULL}) AS ${NewsFields.TAGS}
+  FROM ${Parts.BODY};`,
 
   selectAll: `
                     SELECT
